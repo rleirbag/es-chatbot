4bc6809f2aaff492d840eabca17227ef4c80baba
Gabriel Lima <gabrielrlima@fitec.org.br>
2025-04-26 09:25:30 -0300

    Adiciona funcionalidade de atualização de refresh token para usuários existentes ao criar um novo usuário. Refatora a classe CreateUserUseCase para incluir lógica de verificação e atualização do refresh token, melhorando o tratamento de erros e a legibilidade do código.

--- app/services/users/create_user_use_case.py
+++ app/services/users/create_user_use_case.py
@@ -3,7 +3,7 @@ from typing import Optional, Tuple
 
 from sqlalchemy.orm import Session
 
-from app.config.database import commit, create, get_by_attribute
+from app.config.database import commit, create, get_by_attribute, update
 from app.models.user import User
 from app.schemas.error import Error
 from app.schemas.user import UserCreate
@@ -12,34 +12,55 @@ logger = logging.getLogger(__name__)
 
 
 class CreateUserUseCase:
-
     @staticmethod
     @commit
     def execute(
-            db: Session, user_create: UserCreate
-        ) -> Tuple[Optional[User], Optional[Error]] | None:
-
+        db: Session, user_create: UserCreate
+    ) -> Tuple[Optional[User], Optional[Error]] | None:
         try:
-            logger.info(f"Tentando criar usuário com email: {user_create.email}")
+            logger.info(
+                f'Tentando criar usuário com email: {user_create.email}'
+            )
 
             user = User(**user_create.model_dump())
-            logger.info(f"Usuário criado: {user.__dict__}")
+            logger.info(f'Usuário criado: {user.__dict__}')
 
             existent_user, _ = get_by_attribute(db, User, 'email', user.email)
             if existent_user:
-                logger.info(f"Usuário já existe com email: {user.email}")
+                logger.info(f'Usuário já existe com email: {user.email}')
+
+                if existent_user.refresh_token != user.refresh_token:
+                    user_updated, error = update(
+                        db,
+                        User,
+                        existent_user.id,
+                        refresh_token=user.refresh_token,
+                    )
+
+                    logger.info(
+                        f'Usuário atualizado com novo refresh_token: {user_updated.__dict__}'
+                    )
+
+                    if error:
+                        logger.error(f'Erro ao atualizar usuário: {error}')
+                        return None, error
+
+                    return user_updated, None
+
                 return existent_user, None
 
             user, error = create(db, user)
-            logger.info(f"Resultado da criação: user={user}, error={error}")
+            logger.info(f'Resultado da criação: user={user}, error={error}')
 
             if error:
-                logger.error(f"Erro ao criar usuário: {error}")
+                logger.error(f'Erro ao criar usuário: {error}')
                 return None, error
 
-            logger.info(f"Usuário criado com sucesso: {user.__dict__}")
+            logger.info(f'Usuário criado com sucesso: {user.__dict__}')
             return user, None
 
         except Exception as e:
-            logger.error(f"Erro inesperado ao criar usuário: {str(e)}")
-            return None, Error(error_code=500, error_message=f"Erro inesperado: {str(e)}")
+            logger.error(f'Erro inesperado ao criar usuário: {str(e)}')
+            return None, Error(
+                error_code=500, error_message=f'Erro inesperado: {str(e)}'
+            )
